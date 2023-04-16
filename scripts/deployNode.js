9264db7f2f106fb91a5b2e355dec9cb2a75ffa04
weisiwu <2436887475@qq.com>
2023-04-16 22:31:29 +0800

    chore: 添加杂谈3、4

---
+++ scripts/deployNode.js
@@ -0,0 +1,24 @@
+/** @format */
+
+// /** @format */
+
+// const fs = require('fs');
+// import {deleteAsync} from 'del';
+// var cd = require('cd');
+
+// const deletedFilePaths = await deleteAsync(['db.json']);
+// console.log('Deleted files:\n', 'db.json');
+// var dir = new cd('./public');
+// const deletedFilePaths = await deleteAsync(['db.json']);
+// rm -rf `ls | grep -v .git`
+// var dir = new cd('..');
+
+// console.log('Deleted files:\n', deletedFilePaths.join('\n'));
+// console.log('Deleted directories:\n', deletedDirectoryPaths.join('\n'));
+
+// hexo g -f
+// cd ./public
+// git add .
+// git commit
+// git push
+// cd ..
