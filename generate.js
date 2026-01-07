4c7a08165475fe8333fdd1c890f7d3ccc459eba2
weisiwu <2436887475@qq.com>
2026-01-07 22:43:00 +0800

    feat: 整理脚本

--- generate.js
+++ generate.js
@@ -1,13 +1,19 @@
 /** @format */
 
-// const fs = require('fs');
-const path = require('path');
-const readline = require('readline').createInterface({
+import fs from 'node:fs';
+import path from 'node:path';
+import readline from 'node:readline';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-readline.question('请输入文件名: \n', (name) => {
+rl.question('请输入文件名: \n', (name) => {
   const post_path = path.join(__dirname, '../', 'source/_posts');
   const template_path = path.join(post_path, 'template.md');
   const target_path = path.join(post_path, `${name}.md`);
@@ -16,6 +22,6 @@ readline.question('请输入文件名: \n', (name) => {
     if (!err) {
       console.log(`创建${name}.md成功!`);
     }
-    readline.close();
+    rl.close();
   });
 });
